49c9f77c115f7ec96b01e8dc47487f6fc1eaefe0
Al Evan Castillo <45473100+Evanchii@users.noreply.github.com>
2022-10-13 13:19:07 +0800

    Privacy Policy & Terms of Service
    • Terms of Service
    • Privacy Policy

---
+++ pages/terms.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Terms of Service | FastFare</title>
+
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+
+    <script src="https://kit.fontawesome.com/a2501cd80b.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../styles/master.css">
+
+    <style>
+        .container {
+            background-color: #fff;
+            box-shadow: 0px 0px 7px 0px rgba(0, 0, 0, 0.75);
+            -webkit-box-shadow: 0px 0px 7px 0px rgba(0, 0, 0, 0.75);
+            -moz-box-shadow: 0px 0px 7px 0px rgba(0, 0, 0, 0.75);
+        }
+    </style>
+</head>
+<body style="background: radial-gradient(circle at 75% 0%, #21b5eb, #15c499, #129431);">
+    <div class="container min-w-50 my-5 mx-auto p-4 rounded-3">
+    <h1 style="text-align: center;">Terms of Service</h1>
+    <br>
+    <p>The Service is protected by copyright, trademark, and other laws of both the Country and foreign countries. Our trademarks and trade dress may not be used in connection with any product or service without the prior written consent of the Company.</p>
+
+<h3>Your Feedback to Us</h3>
+
+<p>You assign all rights, title and interest in any Feedback You provide the Company. If for any reason such assignment is ineffective, You agree to grant the Company a non-exclusive, perpetual, irrevocable, royalty free, worldwide right and license to use, reproduce, disclose, sub-license, distribute, modify and exploit such Feedback without restriction.<p>
+
+<h3>Links to Other Websites</h3>
+
+<p>Our Service may contain links to third-party web sites or services that are not owned or controlled by the Company.</p>
+
+<p>The Company has no control over, and assumes no responsibility for, the content, privacy policies, or practices of any third party web sites or services. You further acknowledge and agree that the Company shall not be responsible or liable, directly or indirectly, for any damage or loss caused or alleged to be caused by or in connection with the use of or reliance on any such content, goods or services available on or through any such web sites or services.</p>
+
+<p>We strongly advise You to read the terms and conditions and privacy policies of any third-party web sites or services that You visit.</p>
+
+<h3>Termination</h3>
+
+<p>We may terminate or suspend Your Account immediately, without prior notice or liability, for any reason whatsoever, including without limitation if You breach these Terms and Conditions.</p>
+
+<p>Upon termination, Your right to use the Service will cease immediately. If You wish to terminate Your Account, You may simply discontinue using the Service.</p>
+
+<h3>Limitation of Liability</h3>
+
+<p>Notwithstanding any damages that You might incur, the entire liability of the Company and any of its suppliers under any provision of this Terms and Your exclusive remedy for all of the foregoing shall be limited to the amount actually paid by You through the Service or 100 USD if You haven't purchased anything through the Service.</p>
+
+<p>To the maximum extent permitted by applicable law, in no event shall the Company or its suppliers be liable for any special, incidental, indirect, or consequential damages whatsoever (including, but not limited to, damages for loss of profits, loss of data or other information, for business interruption, for personal injury, loss of privacy arising out of or in any way related to the use of or inability to use the Service, third-party software and/or third-party hardware used with the Service, or otherwise in connection with any provision of</p>
+    </div>
+</body>
+</html>
